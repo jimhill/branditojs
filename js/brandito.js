4d30ee739bc497bc2ad0c23198c8e8670ce68d42
Wiseguy Digital <studio@wiseguydigital.com>
2014-06-17 11:55:48 +0100

    Added RGB values back in

--- js/brandito.js
+++ js/brandito.js
@@ -70,15 +70,39 @@ var Brandito = window.Brandito || (function($){
 
 		var output = "";
 		$.each( data, function( key, val ) {
+			var rgb = hexToRgb(val);
 			output += "<div class='brandito-swatch-boxes'>";
 			output += "<svg width='" + size + "' height='" + size + "'><rect class='square' width='" + size + "' height='" + size + "' style='fill:"+val+";' /></svg>";
 			output += "<span>HEX: <input type='text' value='"+val+"'></span>";
-			output += "<span>RGB: <input type='text' value='0, 0, 0'></span>";
+			output += "<span>RGB: <input type='text' value='" + rgb.r + "," + rgb.g + "," + rgb.b + "'></span>";
 		  	output += "</div>";
         });
   
         return output;
 
+	}
+	
+	// Hex to RGB
+	// Thanks to http://stackoverflow.com/a/5624139/148769
+	function hexToRgb(hex) {
+	    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+	    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+	        return r + r + g + g + b + b;
+	    });
+
+	    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	    return result ? {
+	        r: parseInt(result[1], 16),
+	        g: parseInt(result[2], 16),
+	        b: parseInt(result[3], 16)
+	    } : null;
+	}
+
+	// RGB to Hex
+	// Thanks to http://stackoverflow.com/a/5624139/148769
+	function rgbToHex(r, g, b) {
+    	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 	}
 
 	// Run
